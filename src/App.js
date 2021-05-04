93723aa9c5b7256b9c153d59c1c93f8c36ed4113
kheersagar <santparja@gmail.com>
2021-05-04 08:29:45 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,NavLink,Route, Switch} from "react-router-dom";
 import './App.css';
+import NavigationBar from './component/NavigationBar/NavigationBar';
+
 
 function App() {
   return (
     <div className="App">
-
-    </div>
+    <NavigationBar />
+      <Switch>
+        <Route path="/benefits">
+          <h1>benefits</h1>
+        </Route>
+        <Route path="/goals">
+          <h1>goals</h1>
+        </Route>
+        <Route path="/member">
+          <h1>member</h1>
+        </Route>
+        <Route>
+          <h1>404!</h1>
+        </Route>
+      </Switch>
+  </div>
   );
 }
 
