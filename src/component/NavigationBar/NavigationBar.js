1fede00e2443a59d07f51637dfb4bf852e9d1507
kheersagar <santparja@gmail.com>
2021-05-04 14:40:55 +0530

    logo added

--- src/component/NavigationBar/NavigationBar.js
+++ src/component/NavigationBar/NavigationBar.js
@@ -2,37 +2,41 @@ import React from 'react';
 import {Navbar,Form,Button,FormControl,Nav,NavDropdown} from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import "./NavigationBar.css";
+import logo from "../../Image/logo.png";
 
 function NavigationBar() {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg"  bg="light" variant="light" className="navbar">
-  <Navbar.Brand href="#home" className="nav__left">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto nav__middle">
-    <NavLink to="/benefits" exact activeClassName="nav__active" className="nav-link">
-      Benefits
-    </NavLink>
-    <NavLink to="/goals" exact activeClassName="nav__active" className="nav-link">
-      Goals
-    </NavLink>
-    <NavLink to="/member" exact activeClassName="nav__active" className="nav-link">
-      Member
-    </NavLink>
-    <NavLink to="/blog" exact activeClassName="nav__active" className="nav-link">
-      blog
-    </NavLink>
-    <NavLink to="/team" exact activeClassName="nav__active" className="nav-link">
-      Team
-    </NavLink>
-    </Nav>
-    <Nav className="nav__right">
-      <NavLink activeClassName="nav__active" className="nav-link" to="/member">
-        More deets
-      </NavLink>
-    </Nav>
-  </Navbar.Collapse>
+      <Navbar.Brand href="#home" className="nav__left">
+        <img src={logo} alt="logo" width="100" height="35" style={{marginRight:"10px"}}/> 
+        React-Bootstrap
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto nav__middle">
+        <NavLink to="/benefits" exact activeClassName="nav__active" className="nav-link">
+          Benefits
+        </NavLink>
+        <NavLink to="/goals" exact activeClassName="nav__active" className="nav-link">
+          Goals
+        </NavLink>
+        <NavLink to="/member" exact activeClassName="nav__active" className="nav-link">
+          Member
+        </NavLink>
+        <NavLink to="/blog" exact activeClassName="nav__active" className="nav-link">
+          blog
+        </NavLink>
+        <NavLink to="/team" exact activeClassName="nav__active" className="nav-link">
+          Team
+        </NavLink>
+        </Nav>
+        <Nav className="nav__right">
+          <NavLink activeClassName="nav__active" className="nav-link" to="/member">
+            More deets
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
 </Navbar>
     </div>
   )
