e6ac2104096cb6ccf83fd4ac6dca4181a30bc414
CreatorSam <74644535+CreatorSam@users.noreply.github.com>
2021-05-23 22:53:47 +0530

    added container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,NavLink,Route, Switch} from "react-router-dom";
 import './App.css';
+import Containers from './component/Container/Containers';
 import NavigationBar from './component/NavigationBar/NavigationBar';
 
 
@@ -9,13 +10,13 @@ function App() {
     <div className="App">
     <NavigationBar />
       <Switch>
-        <Route path="/benefits">
-          <h1>benefits</h1>
+        <Route path="/" exact>
+           <Containers />
         </Route>
-        <Route path="/goals">
+        <Route path="/goals" exact>
           <h1>goals</h1>
         </Route>
-        <Route path="/member">
+        <Route path="/member" exact>
           <h1>member</h1>
         </Route>
         <Route>
