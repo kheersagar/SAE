03a9faf3e9a3a4023e51f3896d497ec7aaae302c
shikhar <anshullohiya321@gmail.com>
2021-05-25 16:40:50 +0530

    land

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,NavLink,Route, Switch} from "react-router-dom";
 import './App.css';
+import  './component/Landingpage/Landing.css';
 import NavigationBar from './component/NavigationBar/NavigationBar';
 
+import Landingpage from './component/Landingpage/landing';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
     <NavigationBar />
       <Switch>
         <Route path="/" >
-          <h1>Home</h1>
+          {/* <h1>Home</h1> */}
+          <Landingpage/>
         </Route>
         <Route path="/benefits">
           <h1>benefits</h1>
