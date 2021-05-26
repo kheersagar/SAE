47019e82b6ff5033c2f64cd646a1e4d33ecf0107
kheersagar <santparja@gmail.com>
2021-05-26 12:24:45 +0530

    animation added

--- src/component/Landingpage/landing.js
+++ src/component/Landingpage/landing.js
@@ -1,34 +1,41 @@
 import React from 'react';
+import Aos from "aos";
 
+import "aos/dist/aos.css";
 import logo from "../../Image/baja.jpg";
-
 import logos from "../../Image/vector.jpg";
 
 function landing() {
+  Aos.init({
+    offset: window.innerHeight > 1000 ?  600: 300,
+    duration: 600,
+    easing: 'ease-in-sine',
+    delay: 50,
+  });
   return (
     <>
      <div className="main__main">
     <div className="landing__main">
       <div className="left__main">
         <div>
-          <div className="moto">
+          <div className="moto" data-aos="fade-right">
           SUPPORTING, CONNECTING &<br/>
           DEVELOPING SPORTING DIRECTORS 
           </div>
         </div>
-        <div className="cmp_name">
+        <div className="cmp_name" data-aos="fade-right">
        Association of<br/> Sporting Directors
         </div>
         
       </div>
-      <div className="right__main">
+      <div className="right__main" data-aos="fade-left">
         <h5> MEET THE TEAM</h5>
       
 
       </div>
 
     </div>
-    <div className="image">
+    <div className="image" data-aos="fade-right" data-aos-once="true">
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWYlRr3pfOYD4d-6SQmg8KwXOZckYN1qjfpQ&usqp=CAU" alt="piv" className="img-fluid"/>
     </div>
     </div>
