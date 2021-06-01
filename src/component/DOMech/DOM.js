d559bf15f4f9234f641dd0fca497a28f93ce6c7b
shikhar <anshullohiya321@gmail.com>
2021-06-01 12:16:59 +0530

    card_complete

--- src/component/DOMech/DOM.js
+++ src/component/DOMech/DOM.js
@@ -31,8 +31,8 @@ function DOM() {
                   <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" className="card__profile"/>
               </div>
                 <div className="paragraph">
-                  <h3>this is op jindal univeristy hello in opju</h3>
-                  <p> The Department organizes number of short term training programs, workshop and seminars from time to time for faculties and students for sharing and updating their technical knowledge in a regular and periodic manner.
+               <h3> this is op jindal univeristy hello in opju </h3>
+                  <p className="para"> The Department organizes number of short term training programs, workshop and seminars from time to time for faculties and students for sharing and updating their technical knowledge in a regular and periodic manner.
               
                   </p>
            </div>
