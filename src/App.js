0cdbcddbe803cb4ba605d70218eb8277eb512f19
kheersagar <santparja@gmail.com>
2021-06-05 22:46:03 +0530

    routing issue resolved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router,NavLink,Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router,NavLink,Redirect,Route, Switch} from "react-router-dom";
 import './App.css';
 import  './component/Landingpage/Landing.css';
 import Containers from './component/Container/Containers';
@@ -46,9 +46,7 @@ function App() {
           <Route path="/SAE/member" exact>
             <h1>member</h1>
           </Route>
-          <Route>
-            <h1>404!</h1>
-          </Route>
+          <Redirect to="/SAE" />
         </Switch>
       <Footer />
 
